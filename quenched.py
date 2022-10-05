eb87a94edf9436a758cca8893c968c5f3a072967
Cam Lawlor-Forsyth <clawlorf@uwaterloo.ca>
2022-10-04 23:55:34 -0400

    minor updates

--- quenched.py
+++ quenched.py
@@ -1,4 +1,5 @@
 
+from os.path import exists
 import pickle
 import numpy as np
 
@@ -27,12 +28,16 @@ def determine_quenched_systems(simName, snapNum, mass_bin_edges,
         SFHs = hf['SFH'][:]
         times = hf['times'][:]
     
-    # add empty quenched mask, onset and quench lookbacktimes into the HDF5
+    # add empty quenched mask, onset and termination times into the HDF5
     # file to populate later
-    with h5py.File(infile, 'a') as hf :
-        add_dataset(hf, np.full(len(subIDs), False), 'quenched_mask')
-        add_dataset(hf, np.full(len(subIDs), np.nan), 'onset_times')
-        add_dataset(hf, np.full(len(subIDs), np.nan), 'quench_times')
+    if exists(infile) :
+        with h5py.File(infile, 'a') as hf :
+            if 'quenched_mask' not in hf.keys() :
+                add_dataset(hf, np.full(len(subIDs), False), 'quenched_mask')
+            if 'onset_times' not in hf.keys() :
+                add_dataset(hf, np.full(len(subIDs), np.nan), 'onset_times')
+            if 'termination_times' not in hf.keys() :
+                add_dataset(hf, np.full(len(subIDs), np.nan), 'termination_times')
     
     # loop through the galaxies in the sample
     for i, (subID, mass, SFH) in enumerate(zip(subIDs, masses, SFHs)) :
@@ -44,32 +49,34 @@ def determine_quenched_systems(simName, snapNum, mass_bin_edges,
         # get the corresponding lower and upper two sigma limits for that mass
         lo_SFH, hi_SFH = get_SFH_limits(limits, np.array(mass_bin_edges), mass)
         
-        # now select galaxies that are within the SFR limits then quench
-        lo_diff = smoothed - lo_SFH
-        hi_diff = smoothed - hi_SFH
-        quench_index = np.argmax(lo_diff > 0) - 1 # TODO - update after time conversion
-        # equivalent to np.where(lo_diff > 0)[0][0] - 1
-        
         # set the indices where the limits don't have to be strictly observed,
         # essentially a small tolerance given the smoothing and low SFRs at
         # early times
         start_lim, end_lim = 3, -3 # originally was -5
         
-        # TODO - determine onset index, and onset time
-        maxima, props = find_peaks(smoothed, height=0.4*np.max(smoothed))
-        onset_index = maxima[-1]
+        # now select galaxies that are within the SFR limits then quench
+        lo_diff = smoothed - lo_SFH
+        hi_diff = smoothed - hi_SFH
+        termination_index = np.argmax(lo_diff[start_lim:] < 0) + start_lim
+        # or use np.where(lo_diff[start_lim:] < 0)[0][0] + start_lim
         
         # these are the criteria for the galaxy to be quenched
-        # TODO - update after time conversion
-        if (np.all(lo_diff[:start_lim] <= 0) and np.all(hi_diff[:end_lim] <= 0) and
-            np.all(lo_diff[quench_index+1:end_lim] >= 0)) :
+        if (np.all(lo_diff[end_lim:] <= 0) and
+            np.all(hi_diff[start_lim:] <= 0) and
+            np.all(lo_diff[termination_index:end_lim] <= 0)) :
+            
+            # find the peaks of the smoothed curve
+            maxima, props = find_peaks(smoothed, height=0.4*np.max(smoothed))
+            
+            # use the final peak as the onset of quenching
+            onset_index = maxima[-1]
             
             # if the galaxy is quenched, change the value in the mask, and
-            # update the onset and quench lookbacktimes
+            # update the onset and termination times
             with h5py.File(infile, 'a') as hf :
                 hf['quenched_mask'][i] = True
                 hf['onset_times'][i] = times[onset_index]
-                hf['quench_times'][i] = times[quench_index]
+                hf['termination_times'][i] = times[termination_index]
     
     return
 
@@ -101,21 +108,21 @@ def get_quenched_systems_info(simName, snapNum) :
         SFHs = hf['SFH'][:]
         times = hf['times'][:]
         onset_times = hf['onset_times'][:]
-        quench_times = hf['quench_times'][:]
+        termination_times = hf['termination_times'][:]
     
-    return (subIDs, masses, R_es, SFRs, SFHs, times, onset_times, quench_times,
-            limits)
+    return (subIDs, masses, R_es, SFRs, SFHs, times, onset_times,
+            termination_times, limits)
 
 def plot_quenched_systems(simName, snapNum, mass_bin_edges, window_length,
                           polyorder) : 
     
     # retrieve the relevant information about the quenched systems
     (subIDs, masses, _, _, SFHs, times, onset_times,
-     quench_times, limits) = get_quenched_systems_info(simName, snapNum)
+     termination_times, limits) = get_quenched_systems_info(simName, snapNum)
     
     # loop through the galaxies in the quenched sample
-    for (subID, mass, SFH, onset_time,
-         quench_time) in zip(subIDs, masses, SFHs, onset_times, quench_times) :
+    for (subID, mass, SFH, tonset,
+         tterm) in zip(subIDs, masses, SFHs, onset_times, termination_times) :
         
         # smooth the SFH of the specific galaxy
         smoothed = savgol_filter(SFH, window_length, polyorder)
@@ -125,20 +132,19 @@ def plot_quenched_systems(simName, snapNum, mass_bin_edges, window_length,
         lo_SFH, hi_SFH = get_SFH_limits(limits, np.array(mass_bin_edges), mass)
         
         # now plot the curves
-        outfile = 'output/quenched_SFHs/quenched_SFH_subID_{}.png'.format(subID)
-        plt.plot_simple_multi_with_times([times, times,
-                                          times, times],
+        outfile = 'output/quenched_SFHs(t)/quenched_SFH_subID_{}.png'.format(subID)
+        plt.plot_simple_multi_with_times([times, times, times, times],
                                          [SFH, smoothed, lo_SFH, hi_SFH],
                                          ['data', 'smoothed', 'lo, hi', ''],
                                          ['grey', 'k', 'lightgrey', 'lightgrey'],
                                          ['', '', '', ''],
                                          ['--', '-', '-.', '-.'],
                                          [0.5, 1, 1, 1],
-                                         np.nan, onset_time, quench_time,
-                                         xlabel=r'$t_{\rm lookback}$ (Gyr)',
+                                         np.nan, tonset, tterm,
+                                         xlabel=r'$t$ (Gyr)',
                                          ylabel=r'SFR ($M_{\odot}$ yr$^{-1}$)',
-                                         xmin=-0.1, xmax=13.8,
-                                         scale='linear', save=True, outfile=outfile)
+                                         xmin=-0.1, xmax=13.8, scale='linear',
+                                         save=True, outfile=outfile, loc=1)
     
     return
 
@@ -164,24 +170,24 @@ def plot_quenched_systems_in_massBin(simName, snapNum, mass_bin_edges,
         # TODO - also plot the median of the quenched SFHs in the mass bin
         
         # plot and save the SFMS in each mass bin
-        outfile = 'output/quenched_SFHs_in_massBin/massBin_{}.png'.format(i)
+        outfile = 'output/quenched_SFHs_in_massBin(t)/massBin_{}.png'.format(i)
         plt.plot_simple_many(times, smoothed_SFHs,
-                             xlabel=r'$t_{\rm lookback}$ (Gyr)',
+                             xlabel=r'$t$ (Gyr)',
                              ylabel=r'SFR ($M_{\odot}$ yr$^{-1}$)',
                              xmin=-0.1, xmax=13.8, save=True, outfile=outfile)
     
     return
 
-def plot_quenching_times(simName, snapNum) :
+def plot_termination_times(simName, snapNum) :
     
     # retrieve the relevant information about the quenched systems
     (_, masses, _, _, _, _, _,
-     quench_times, _) = get_quenched_systems_info(simName, snapNum)
+     termination_times, _) = get_quenched_systems_info(simName, snapNum)
     
     # plot the quenching time as a function of stellar mass
-    plt.plot_scatter(masses, quench_times, 'k', 'data', 'o',
+    plt.plot_scatter(masses, termination_times, 'k', 'data', 'o',
                      xlabel=r'$\log(M_{*}/M_{\odot})$',
-                     ylabel=r'$t_{\rm lookback, quench}$ (Gyr)')
+                     ylabel=r'$t_{\rm termination}$ (Gyr)')
     
     return
 
@@ -190,7 +196,7 @@ def reduce_SFH_to_quenched_systems(simName, snapNum) :
     # define the input directory and the input file for the SFHs
     inDir = bsPath(simName)
     infile = inDir + '/{}_{}_sample_SFHs(t).hdf5'.format(simName, snapNum)
-    outfile = inDir + '/{}_{}_quenched_SFHs(t)s.hdf5'.format(simName, snapNum)
+    outfile = inDir + '/{}_{}_quenched_SFHs(t).hdf5'.format(simName, snapNum)
     
     # read the input file and populate the information
     with h5py.File(infile, 'r') as hf :
@@ -207,7 +213,7 @@ def reduce_SFH_to_quenched_systems(simName, snapNum) :
         R_es = hf['SubhaloHalfmassRadStars'][:]
         SFHs = hf['SFH'][:]
         onset_times = hf['onset_times'][:]
-        quench_times = hf['quench_times'][:]
+        termination_times = hf['termination_times'][:]
         quenched_mask = hf['quenched_mask'][:]
     
     # mask and copy that information to the new file
@@ -225,6 +231,6 @@ def reduce_SFH_to_quenched_systems(simName, snapNum) :
         add_dataset(hf, R_es[quenched_mask], 'SubhaloHalfmassRadStars')
         add_dataset(hf, SFHs[quenched_mask], 'SFH')
         add_dataset(hf, onset_times[quenched_mask], 'onset_times')
-        add_dataset(hf, quench_times[quenched_mask], 'quench_times')
+        add_dataset(hf, termination_times[quenched_mask], 'termination_times')
     
     return
